54c557725f8a6c0ad8125597b568652854432071
yusukerobot <150100784+yusukerobot@users.noreply.github.com>
2025-01-01 16:57:36 +0900

    create sbx test

--- pareto_analysis/scripts/adjust/pareto_adjust.py
+++ pareto_analysis/scripts/adjust/pareto_adjust.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 # 元のCSVファイルのパス
-input_file_path = '../../data/raw/data1.csv'
+input_file_path = '../../data/raw/sbx/eta50.csv'
 
 # 新しいCSVファイルのパス
-output_file_path = '../../data/pareto1.csv'
+output_file_path = '../../data/sbx/eta50.csv'
 
 def process_csv(input_path, output_path):
     # 新しいCSV内容を保存するリスト
